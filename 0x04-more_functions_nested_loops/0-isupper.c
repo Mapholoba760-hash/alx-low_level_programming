d8a84a1c0907729f35c6c097317bb61561a49bda
Mapholoba760-hash <ngcobo.brian5@gmail.com>
2023-07-21 03:45:17 -0700

    ispper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper checks if a character is upper case
+ * returns 1 if uppercase and 0 otherwise
+ */
+
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90 )
+	{
+		return (1);
+	}
+	return (0);
+}
