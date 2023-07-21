e9297ee79a8eca959701de769717059358685a90
Mapholoba760-hash <ngcobo.brian5@gmail.com>
2023-07-21 04:14:23 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * isdigit checks if the input is a digit form 0 through 9
+ * return 1 if is digit and 0 otherwise
+*/
+
+int _isdigit(intx)
+{
+	if (x 48 >= && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
