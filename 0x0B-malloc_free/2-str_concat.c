612271f1f4aa31a9a1453c45c2162eb983a2141e
Mapholoba760-hash <ngcobo.brian5@gmail.com>
2023-08-08 16:22:16 -0700

    Malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *conct;
 	int i, ci;
 
-	if (s1 == NULL)
+	if(s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
+		else if(s2 == NULL)
 		s2 = "";
 		i = ci = 0;
 
